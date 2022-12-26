c68dbad7c2d7123d713400c8615f57ff03cfb557
nikitaabramov.dev <abramov.mykyta@gmail.com>
2022-12-26 07:17:44 +0200

    add plugins change functions.php

--- wp-content/themes/test/functions.php
+++ wp-content/themes/test/functions.php
@@ -12,6 +12,132 @@ if ( ! defined( '_S_VERSION' ) ) {
 	define( '_S_VERSION', '1.0.0' );
 }
 
+/* defines START */
+define( '_TP_', get_stylesheet_directory_uri() ); //theme path
+define( '_IMAGES_', _TP_.'/images' ); //images path
+/* defines END */
+
+
+
+/* REMOVE EMOJI START */
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+/* REMOVE EMOJI END */
+
+/* REMOVE GUTENBERG STYLES START */
+function smartwp_remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+}
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css' );
+/* REMOVE GUTENBERG STYLES END */
+
+
+
+/* ACF theme options START */
+if( function_exists('acf_add_options_page') ) {
+    acf_add_options_page(
+        array(
+            'page_title' => 'Options',
+            'menu_title' => 'Options',
+            'menu_slug' => 'theme-options',
+            'capability' => 'edit_posts',
+            'parent_slug' => '',
+            'position' => false,
+        )
+    );
+}
+/* ACF theme options END */
+
+
+
+/* hide admin TABS START */
+function remove_menu () 
+{
+   remove_menu_page('edit-comments.php');
+} 
+
+add_action('admin_menu', 'remove_menu');
+/* hide admin TABS END */
+
+/* body class START */
+function add_slug_body_class( $classes ) {
+    global $post;
+    if ( isset( $post ) ) {
+        $classes[] = $post->post_type . '-' . $post->post_name;
+        $classes[] = 'template-' . str_replace(".php","",get_page_template_slug());
+    }
+    return $classes;
+}
+add_filter( 'body_class', 'add_slug_body_class' );
+/* body class END */
+
+
+/* insert image START */
+function insertImage($file, $class = '', $width = 100, $height = 100, $return = 0) {
+    
+    if (!empty($file)) {
+
+        if(!is_array($file)) {
+            $file_url = _IMAGES_.'/'.$file;
+            $file_title =  pathinfo($file, PATHINFO_FILENAME);
+            $extension = pathinfo($file, PATHINFO_EXTENSION);
+        } else {
+            $file_url = $file['url'];
+            $file_title = $file['alt'];
+            $extension = pathinfo($file['filename'], PATHINFO_EXTENSION);
+        }
+
+        $context = stream_context_create(array (
+            'http' => [
+                'header' => 'Authorization: Basic ' . base64_encode("demo:a30599b78355")
+            ],
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ],
+        ));
+
+        if (!@file_get_contents($file_url, false, $context) === false) {
+            if ($extension == 'svg') {
+                $content = file_get_contents($file_url, false, $context);
+                $content = str_replace('<?xml version="1.0" encoding="UTF-8"?>', '', $content);
+                if ($return) {
+                    return $content;
+                }
+
+                echo $content;
+            } else {
+                $content = '<img 
+                    class="'.$class.'" 
+                    src="'.$file_url. '" 
+                    alt="'.$file_title.'"
+                    width="'.$width.'"
+                    height="'.$height.'" 
+                />';
+
+                if ($return) {
+                    return $content;
+                }
+
+                echo $content;
+            }
+        }
+    }
+}
+/* insert image END */
+
+/* dump START */
+function dd($arr) {
+    echo '<pre>';
+    var_dump($arr);
+    echo '</pre>';
+    die;
+}
+/* dump END */
+
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
